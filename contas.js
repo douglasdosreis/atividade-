fc7e10947f161d67f6d37d0cd0e545868bb58522
douglasdosreis <douglas.reis@sp.senai.br>
2022-02-26 15:57:31 -0300

    javascript

---
+++ contas.js
@@ -0,0 +1,9 @@
+//cálculos
+var valorum = 10;
+var valordois = 2;
+var total;
+//total = valorum + valordois;
+//total = valorum - valordois;
+//total = valorum ** valordois;
+total = valorum % valordois;
+console.log(total);
---
+++ nome.js
@@ -0,0 +1,7 @@
+//console.log('Douglas');
+// primeiro programa
+ var nome = 'Douglas';
+ var sobrenome = 'dos Reis';
+ var idade = 48;
+ var nomecompleto = nome + sobrenome+ idade;
+ console.log(nomecompleto);
